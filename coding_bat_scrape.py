650deb9869a5f7810cac517be7f4be390e951a9d
paolo215 <paolo_villanueva215@yahoo.com>
2017-06-20 14:03:48 -0400

    Coding bat scraping init

---
+++ coding_bat_scrape.py
@@ -0,0 +1,8 @@
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+import requests
+
+
+# Scrape codingbat Java questions
+# Goal: Extract the name of the problem, description, and examples
+
