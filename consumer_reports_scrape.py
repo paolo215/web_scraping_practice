eac58901fa52bb0fe9983302a0d08ec87d13239a
paolo215 <paolo_villanueva215@yahoo.com>
2017-06-20 12:59:47 -0400

    Consumer reports scraping

---
+++ consumer_reports_scrape.py
@@ -0,0 +1,30 @@
+import requests
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+import os
+
+filename = "consumer_reports.txt"
+
+def prep():
+    url = "http://www.consumerreports.org/cro/a-to-z-index/products/index.htm"
+    user_agent = UserAgent()
+    chrome = user_agent.chrome
+    headers = { "headers" : chrome }
+    page = requests.get(url, headers=headers)
+    with open(filename, "w") as f:
+        f.write(page.content)
+
+prep()
+
+fileObj = open(filename, "r")
+contents = fileObj.read()
+fileObj.close()
+
+bsoup = BeautifulSoup(contents, "lxml")
+
+all_divs = bsoup.find_all("div", attrs={"class": "entry-letter"})
+
+for div in all_divs:
+    print(div.div.a.span.string)
+
+
