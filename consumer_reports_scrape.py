a1f108fbdc83d868867d352623ba631d7f1eb1bb
paolo215 <paolo_villanueva215@yahoo.com>
2017-06-20 14:01:29 -0400

    Added some comments for consumer_reports_scrape.py

--- consumer_reports_scrape.py
+++ consumer_reports_scrape.py
@@ -3,9 +3,15 @@ from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 import os
 
+
+# GOAL: Obtain all the products and its corresponding url link
+
 filename = "consumer_reports.txt"
 
 def prep():
+    """
+    Get contents of this page and put it in a text file
+    """
     url = "http://www.consumerreports.org/cro/a-to-z-index/products/index.htm"
     user_agent = UserAgent()
     chrome = user_agent.chrome
@@ -14,7 +20,9 @@ def prep():
     with open(filename, "w") as f:
         f.write(page.content)
 
-prep()
+
+
+# prep()
 
 fileObj = open(filename, "r")
 contents = fileObj.read()
@@ -22,8 +30,8 @@ fileObj.close()
 
 bsoup = BeautifulSoup(contents, "lxml")
 
+# Key = product name, value = url link
 products = {}
-
 products = {div.div.a.span.string: div.div.a["href"] for div in bsoup.find_all("div", class_="entry-letter")}
 
 
