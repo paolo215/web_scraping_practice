6009306863e24dd59143740733d0651fd0d6c61e
paolo215 <paolo_villanueva215@yahoo.com>
2017-06-20 16:06:40 -0400

    Modified coding bat scrape to write data to xlsx

--- coding_bat_scrape.py
+++ coding_bat_scrape.py
@@ -3,64 +3,96 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 import re
+from xlsxwriter import Workbook
 
 # Scrape codingbat Java questions
 # Goal: Extract the name of the problem, description, and examples
 
-user_agent = UserAgent()
-headers = {"headers" : user_agent.chrome}
-main_url = "http://codingbat.com"
-codingbat_java_contents = requests.get(main_url + "/java", headers=headers).content
+def scrape():
+    user_agent = UserAgent()
+    headers = {"headers" : user_agent.chrome}
+    main_url = "http://codingbat.com"
+    codingbat_java_contents = requests.get(main_url + "/java", headers=headers).content
+    bsoup_codingbat_java = BeautifulSoup(codingbat_java_contents, "lxml")
+    divs_summ = bsoup_codingbat_java.find_all("div", class_="summ")
 
-bsoup_codingbat_java = BeautifulSoup(codingbat_java_contents, "lxml")
+    challenges = []
+    for div in divs_summ:
+        section_url = div.a["href"]
+        section_name = div.span.string
 
-divs_summ = bsoup_codingbat_java.find_all("div", class_="summ")
+        # Open section
+        codingbat_java_section_contents = requests.get(main_url + section_url, headers=headers).content    
 
-for div in divs_summ:
-    section_url = div.a["href"]
-    section_name = div.span.string
+        # Create BeautifulSoup for this section
+        bsoup_codingbat_java_section = BeautifulSoup(codingbat_java_section_contents, "lxml")
 
-    # Open section
-    codingbat_java_section_contents = requests.get(main_url + section_url, headers=headers).content    
+        # Get all questions. All questions have td tags
+        java_questions_div = bsoup_codingbat_java_section.find("div", class_="tabin")
+        java_questions_table = java_questions_div.table
+        java_questions_entries = java_questions_table.find_all("td")
 
-    # Create BeautifulSoup for this section
-    bsoup_codingbat_java_section = BeautifulSoup(codingbat_java_section_contents, "lxml")
+        # Go over each questions and extract description and examples
+        for java_question in java_questions_entries:
 
-    # Get all questions. All questions have td tags
-    java_questions_div = bsoup_codingbat_java_section.find("div", class_="tabin")
-    java_questions_table = java_questions_div.table
-    java_questions_entries = java_questions_table.find_all("td")
+            # Get url to the question
+            java_url = java_question.a["href"]
 
-    # Go over each questions and extract description and examples
-    for java_question in java_questions_entries:
+            # Get title of this question
+            java_question_title = java_question.a.string
 
-        # Get url to the question
-        java_url = java_question.a["href"]
+            # Get contents of this web page and create BeautifulSoup
+            codingbat_java_question = requests.get(main_url + java_url, headers=headers).content
+            bsoup_codingbat_java_question = BeautifulSoup(codingbat_java_question, "lxml")
 
-        # Get title of this question
-        java_question_title = java_question.a.string
-
-        # Get contents of this web page and create BeautifulSoup
-        codingbat_java_question = requests.get(main_url + java_url, headers=headers).content
-        bsoup_codingbat_java_question = BeautifulSoup(codingbat_java_question, "lxml")
-
-        # Get description and examples
-        # Description and examples are in td tags
-        java_question_div = bsoup_codingbat_java_question.find("div", class_="tabin")
-        java_question_table = java_question_div.table
-        java_question_td = java_question_table.find("td")
-        java_question_description = java_question_td.div.string
-
-
-        java_question_children = java_question_td.children
-        java_question_examples = []
-   
-        # Get examples using regex
-        regex = re.compile(java_question_title)
-        for child in java_question_children:
-            if regex.match(child.encode("utf-8")):
-                java_question_examples.append(child)
+            # Get description and examples
+            # Description and examples are in td tags
+            java_question_div = bsoup_codingbat_java_question.find("div", class_="tabin")
+            java_question_table = java_question_div.table
+            java_question_td = java_question_table.find("td")
+            java_question_description = java_question_td.div.string
 
 
-        print(java_question_title, java_question_description, java_question_examples)
+            java_question_children = java_question_td.children
+            java_question_examples = []
+       
+            # Get examples using regex
+            regex = re.compile(java_question_title)
+            for child in java_question_children:
+                if regex.match(child.encode("utf-8")):
+                    java_question_examples.append(child)
+
+            data = [java_question_title, java_question_description, java_url] + java_question_examples
+            challenges.append(data)
+
+    return challenges
+
+
+# Put them in worksheet
+def write_to_excel(data):
+    workbook = Workbook("codingbat_challenges.xlsx")
+    worksheet = workbook.add_worksheet()
+
+    # Headers
+    worksheet.write(0, 0, "Title")
+    worksheet.write(0, 1, "Description")
+    worksheet.write(0, 2, "URL")
+    worksheet.write(0, 3, "Questions")
+    for i in range(len(data)):
+        challenge = data[i]
+        for j in range(len(challenge)):
+            worksheet.write(i+1, j, challenge[j])
+
+    workbook.close()
+
+def main():
+    challenges = scrape()
+    write_to_excel(challenges)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
 
