042a6f93a7687a80044ad2b184f7d82b841f1dd3
paolo215 <paolo_villanueva215@yahoo.com>
2017-06-19 22:17:55 -0400

    Bsoup 8 - searching for tags

---
+++ bsoup_8_searching.py
@@ -0,0 +1,38 @@
+from bs4 import BeautifulSoup
+import re
+
+def read_file():
+    fileObj = open("bsoup_2.html")
+    data = fileObj.read()
+    fileObj.close()
+    return data
+
+bsoup = BeautifulSoup(read_file(), "lxml")
+
+# Searching for tags
+# find() and find_all()
+# print(bsoup.find("p"))
+# print(bsoup.find_all("p"))
+
+
+# Use regex to find tags
+# regex = re.compile("^p")
+# for tag in bsoup.find_all(regex):
+#    print(tag.name)
+
+
+# Find multiple tags
+# for tag in bsoup.find_all(["p", "div"]):
+#     print(tag.name)
+
+
+def has_class(tag):
+    return tag.has_attr("class")
+
+# Pass in function
+# for tag in bsoup.find_all(has_class):
+#     print(tag.name)
+
+
+
+
